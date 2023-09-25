18df59d0b67eb0dda55115c3d31929885b52e05a
daniel-dunsin <adejaredaniel12@gmail.com>
2023-09-25 07:14:54 +0100

    fix: changed entry file

--- dist/entry/server.entry.js
+++ dist/entry/server.entry.js
@@ -3,21 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const settings_1 = __importDefault(require("../constants/settings"));
 const logger_1 = __importDefault(require("../helpers/logger"));
 const port = 3001;
 const serverEntry = (app) => {
-    app.listen(port, () => {
-        logger_1.default.info(`Server is listening on port ${port}`);
+    mongoose_1.default
+        .connect(settings_1.default.mongoDbUrl)
+        .then(() => {
+        app.listen(port, () => {
+            logger_1.default.info(`Server is listening on port ${port}`);
+        });
+    })
+        .catch((error) => {
+        logger_1.default.error(error);
     });
-    // mongoose
-    //   .connect(settings.mongoDbUrl)
-    //   .then(() => {
-    //     app.listen(port, () => {
-    //       logger.info(`Server is listening on port ${port}`);
-    //     });
-    //   })
-    //   .catch((error) => {
-    //     logger.error(error);
-    //   });
 };
 exports.default = serverEntry;
