37ce0195c0cf4338c2499ddb617ac804fa46bdd5
daniel-dunsin <adejaredaniel12@gmail.com>
2023-11-21 18:44:12 +0100

    fix:deployment fix

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import rateLimiter from "express-rate-limit";
 import serverEntry from "./entry/server.entry";
 import { errorHandler, notFoundError } from "./handlers/error.handlers";
 import routes from "./routes";
@@ -16,12 +15,7 @@ app.use(cors());
 app.use(helmet());
 
 const TEN_MINUTES = 1000 * 60 * 10;
-app.use(
-  rateLimiter({
-    windowMs: TEN_MINUTES,
-    max: 100,
-  })
-);
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 
