e7adc62228fd0be9573c18531374e0f6e70e49d9
Dalaktronixs <hakeemabdullah87@gmail.com>
2024-11-10 15:39:20 +0100

    UPDATE: update code in the routes/index.ts and app.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import auth from './auth.routes';
 import questions from './questions.routes';
 import user from './user.routes';
 import inventory from './inventory.routes';
+import otp from './otp.routes';
 
 const routes = {
   auth,
   questions,
   user,
   inventory,
+  otp,
 };
 
 export default routes;
